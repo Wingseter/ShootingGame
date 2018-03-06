8e05101171f5aeb5ec945b4bf4d1882648143e29
YongHoon Kwon <wingseter@gmail.com>
2018-03-06 15:03:36 +0900

    input keyboard

--- main.cpp
+++ main.cpp
@@ -17,12 +17,13 @@ HDC hdc;
 TCHAR ch = ' ';
 RECT rect;
 PAINTSTRUCT ps;
+bool vkKeys[256];
 
 // 상수
 const char CLASS_NAME[] = "Shooting";
 const char APP_TITLE[] = "Shooting game";
 const int WINDOW_WIDTAH = 400;
-const int WINDOW_HEIGHT = 300;
+const int WINDOW_HEIGHT = 400;
 
 int WINAPI WinMain(HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
@@ -34,6 +35,9 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	// 윈도우 생성
 	if (!CreateMainWindow(hInstance, nCmdShow))
 		return false;
+
+	for (int i = 0; i < 256; i++)
+		vkKeys[i] = false;
 
 	int done = 0;
 	// 메인 루프
@@ -54,11 +58,75 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
 LRESULT WINAPI WinProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
+	short nVirtKey;
+	const short SHIFTED = (short)0x8000;
+	TEXTMETRIC tm;
+	DWORD chWidth = 20;
+	DWORD chHeight = 20;
+
 	switch (msg)
 	{
+	case WM_CREATE:
+		hdc = GetDC(hwnd);
+		GetTextMetrics(hdc, &tm);
+		ReleaseDC(hwnd, hdc);
+		chWidth = tm.tmAveCharWidth;
+		chHeight = tm.tmHeight;
+		return 0;
+
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		return 0;
+	case WM_KEYDOWN:
+		vkKeys[wParam] = true;
+		switch (wParam)
+		{
+		case VK_SHIFT:
+			nVirtKey = GetKeyState(VK_LSHIFT);
+			if (nVirtKey & SHIFTED)
+				vkKeys[VK_LSHIFT] = true;
+			nVirtKey = GetKeyState(VK_RSHIFT);
+			if (nVirtKey & SHIFTED)
+				vkKeys[VK_RSHIFT] = true;
+			break;
+		case VK_CONTROL:
+			nVirtKey = GetKeyState(VK_LCONTROL);
+			if (nVirtKey & SHIFTED)
+				vkKeys[VK_LCONTROL] = true;
+			nVirtKey = GetKeyState(VK_RCONTROL);
+			if (nVirtKey & SHIFTED)
+				vkKeys[VK_RCONTROL] = true;
+			break;
+		}
+		InvalidateRect(hwnd, NULL, TRUE);
+		return 0;
+		break;
+
+	case WM_KEYUP:
+		vkKeys[wParam] = false;
+		switch (wParam)
+		{
+		case VK_SHIFT:
+			nVirtKey = GetKeyState(VK_LSHIFT);
+			if ((nVirtKey &SHIFTED) == 0)
+				vkKeys[VK_RSHIFT] = false;
+			nVirtKey = GetKeyState(VK_RSHIFT);
+			if ((nVirtKey & SHIFTED) == 0)
+				vkKeys[VK_RSHIFT] = false;
+			break;
+		case VK_CONTROL:
+			nVirtKey = GetKeyState(VK_LCONTROL);
+			if ((nVirtKey & SHIFTED) == 0)
+				vkKeys[VK_LCONTROL] = false;
+			nVirtKey = GetKeyState(VK_RCONTROL);
+			if ((nVirtKey & SHIFTED) == 0)
+				vkKeys[VK_RCONTROL] = false;
+			break;
+		}
+		InvalidateRect(hwnd, NULL, TRUE);
+		return 0;
+		break;
+
 	case WM_CHAR:
 		switch (wParam)
 		{
@@ -67,7 +135,6 @@ LRESULT WINAPI WinProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		case 0x0A:	// 줄바꿈
 		case 0x0D:	// 케리지 리턴
 		case 0x1B:	// esc
-			MessageBeep((UINT)-1);
 			return 0;
 		default:
 			ch = (TCHAR)wParam;
@@ -76,8 +143,23 @@ LRESULT WINAPI WinProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		}
 	case WM_PAINT:
 		hdc = BeginPaint(hwnd, &ps);
-		GetClientRect(hwnd, &rect);
-		TextOut(hdc, rect.right / 2, rect.bottom / 2, &ch, 1);
+		TextOut(hdc, 0, 0, &ch, 1);
+
+		for (int r = 0; r < 16; r++)
+		{
+			for (int c = 0; c < 16; c++)
+			{
+				if (vkKeys[r * 16 + c])
+				{
+					SetBkMode(hdc, OPAQUE);
+					TextOut(hdc, c * chWidth + chWidth * 2, r * chHeight + chHeight * 2, "T ", 2);
+				}
+				else {
+					SetBkMode(hdc, TRANSPARENT);
+					TextOut(hdc, c * chWidth + chWidth * 2, r * chHeight + chHeight * 2, "F ", 2);
+				}
+			}
+		}
 		EndPaint(hwnd, &ps);
 		return 0;
 
