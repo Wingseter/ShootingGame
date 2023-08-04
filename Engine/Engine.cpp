571ae3e08599c3d9c99a6fe753bf6d566d177d16
kwon <wingseter@gmail.com>
2023-08-05 00:25:31 +0900

    Create Engine Class and need Others for Engine

---
+++ Engine/Engine.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+#include "Engine.h"
+
+void Engine::Init(const WindowInfo& info)
+{
+	_window = info;
+	ResizeWindow(info.width, info.height);
+
+	_viewport = { 0, 0, static_cast<FLOAT>(info.width), static_cast<FLOAT>(info.height), 0.0f, 1.0f };
+	_scissorRect = CD3DX12_RECT(0, 0, info.width, info.height);
+}
+
+void Engine::Render()
+{
+}
+
+void Engine::ResizeWindow(int32 width, int32 height)
+{
+	_window.width = width;
+	_window.height = height;
+	RECT rect = { 0, 0, width, height };
+	::AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, false);
+	::SetWindowPos(_window.hwnd, 0, 100, 100, width, height, 0);
+}
