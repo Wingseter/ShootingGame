5eae50a7c91c6200e7a7a2c3cf1554b0793cb806
kwon <wingseter@gmail.com>
2023-08-09 23:27:44 +0900

    Init SwapChain Class

--- Engine/SwapChain.cpp
+++ Engine/SwapChain.cpp
@@ -1,2 +1,12 @@
 #include "pch.h"
 #include "SwapChain.h"
+
+void SwapChain::Init(const WindowInfo& info, ComPtr<IDXGIFactory> dxgi, ComPtr<ID3D12CommandQueue> cmdQueue)
+{
+	_swapChain.Reset();
+
+	DXGI_SWAP_CHAIN_DESC sd;
+	sd.BufferDesc.Width = static_cast<uint32>(info.width);
+	sd.BufferDesc.Height = static_cast<uint32>(info.height);
+
+}
