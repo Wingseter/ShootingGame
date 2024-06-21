4a215a8a53e9efaf13d2fe9404648aef689d893b
kwon <wingseter@gmail.com>
2024-06-21 22:14:09 +0900

    Create View
    Now We can create view in constant buffer

--- Engine/ConstantBuffer.cpp
+++ Engine/ConstantBuffer.cpp
@@ -18,7 +18,6 @@ ConstantBuffer::~ConstantBuffer()
 }
 
 
-
 void ConstantBuffer::Init(uint32 size, uint32 count)
 {
 	// 상수 버퍼는 256 바이트 배수로 만들어야 한다
@@ -27,25 +26,66 @@ void ConstantBuffer::Init(uint32 size, uint32 count)
 	_elementCount = count;
 
 	CreateBuffer();
+	CreateView();
 }
 
 void ConstantBuffer::CreateBuffer()
 {
+	// Calculate the buffer size (element size * element count)
 	uint32 bufferSize = _elementSize * _elementCount;
+
+	// Set heap properties (upload heap type)
 	D3D12_HEAP_PROPERTIES heapProperty = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
+
+	// Set resource description (buffer type, buffer size)
 	D3D12_RESOURCE_DESC desc = CD3DX12_RESOURCE_DESC::Buffer(bufferSize);
 
+	// Create a committed resource (create buffer resource in upload heap)
 	DEVICE->CreateCommittedResource(
-		&heapProperty,
-		D3D12_HEAP_FLAG_NONE,
-		&desc,
-		D3D12_RESOURCE_STATE_GENERIC_READ,
-		nullptr,
-		IID_PPV_ARGS(&_cbvBuffer));
+		&heapProperty,                     // Heap properties
+		D3D12_HEAP_FLAG_NONE,              // Heap flags (none)
+		&desc,                             // Resource description
+		D3D12_RESOURCE_STATE_GENERIC_READ, // Initial resource state (generic read)
+		nullptr,                           // Clear value (none)
+		IID_PPV_ARGS(&_cbvBuffer));        // Store created resource in _cbvBuffer
 
+	// Map the buffer (map to CPU memory space)
 	_cbvBuffer->Map(0, nullptr, reinterpret_cast<void**>(&_mappedBuffer));
-	// We do not need to unmap until we are done with the resource.  However, we must not write to
-	// the resource while it is in use by the GPU (so we must use synchronization techniques).
+
+	// No need to unmap until done with the resource. However, we must not write to
+	// the resource while it is in use by the GPU (synchronization techniques must be used).
+}
+
+
+
+void ConstantBuffer::CreateView()
+{
+	// Create a descriptor heap for the constant buffer views (CBVs)
+	D3D12_DESCRIPTOR_HEAP_DESC cbvDesc = {};
+	cbvDesc.NumDescriptors = _elementCount;  // Number of descriptors to allocate
+	cbvDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;  // No specific flags
+	cbvDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;  // Descriptor heap type for CBV, SRV, and UAV
+	DEVICE->CreateDescriptorHeap(&cbvDesc, IID_PPV_ARGS(&_cbvHeap));  // Create the descriptor heap
+
+	// Get the CPU descriptor handle for the start of the heap
+	_cpuHandleBegin = _cbvHeap->GetCPUDescriptorHandleForHeapStart();
+	// Get the size of each descriptor in the heap
+	_handleIncrementSize = DEVICE->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
+
+	// Loop through each element to create individual constant buffer views (CBVs)
+	for (uint32 i = 0; i < _elementCount; ++i)
+	{
+		// Get the CPU descriptor handle for the current index
+		D3D12_CPU_DESCRIPTOR_HANDLE cbvHandle = GetCpuHandle(i);
+
+		// Set up the constant buffer view description
+		D3D12_CONSTANT_BUFFER_VIEW_DESC cbvDesc = {};
+		cbvDesc.BufferLocation = _cbvBuffer->GetGPUVirtualAddress() + static_cast<uint64>(_elementSize) * i;  // GPU virtual address for the buffer
+		cbvDesc.SizeInBytes = _elementSize;  // Size of the buffer in bytes
+
+		// Create the constant buffer view in the descriptor heap
+		DEVICE->CreateConstantBufferView(&cbvDesc, cbvHandle);
+	}
 }
 
 void ConstantBuffer::Clear()
@@ -55,18 +95,32 @@ void ConstantBuffer::Clear()
 
 void ConstantBuffer::PushData(int32 rootParamIndex, void* buffer, uint32 size)
 {
+	// Ensure the current index is within the valid range (bounds check)
 	assert(_currentIndex < _elementSize);
 
+	// Copy the data to the mapped buffer at the current index position
 	::memcpy(&_mappedBuffer[_currentIndex * _elementSize], buffer, size);
 
+	// Get the GPU virtual address for the current index position
 	D3D12_GPU_VIRTUAL_ADDRESS address = GetGpuVirtualAddress(_currentIndex);
+
+	// Set the graphics root constant buffer view (root parameter index and address)
 	CMD_LIST->SetGraphicsRootConstantBufferView(rootParamIndex, address);
+
+	// Increment the current index (move to the next position)
 	_currentIndex++;
 }
+
+
 
 D3D12_GPU_VIRTUAL_ADDRESS ConstantBuffer::GetGpuVirtualAddress(uint32 index)
 {
 	D3D12_GPU_VIRTUAL_ADDRESS objCBAddress = _cbvBuffer->GetGPUVirtualAddress();
 	objCBAddress += index * _elementSize;
 	return objCBAddress;
+}
+
+D3D12_CPU_DESCRIPTOR_HANDLE ConstantBuffer::GetCpuHandle(uint32 index)
+{
+	return CD3DX12_CPU_DESCRIPTOR_HANDLE(_cpuHandleBegin, index * _handleIncrementSize)
 }
--- Engine/RootSignature.cpp
+++ Engine/RootSignature.cpp
@@ -1,17 +1,32 @@
 #include "pch.h"
 #include "RootSignature.h"
 
+// RootSignature 클래스의 Init 함수 정의
 void RootSignature::Init(ComPtr<ID3D12Device> device)
 {
-	CD3DX12_ROOT_PARAMETER param[2];
-	param[0].InitAsConstantBufferView(0); // 0번 -> b0 -> CBV 
-	param[1].InitAsConstantBufferView(1); // 1번 -> b1 -> CBV
+    // 루트 파라미터 배열 선언 (2개의 파라미터를 가진다)
+    CD3DX12_ROOT_PARAMETER param[2];
 
-	D3D12_ROOT_SIGNATURE_DESC sigDesc = CD3DX12_ROOT_SIGNATURE_DESC(2, param);
-	sigDesc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT; // 입력 조립기 단계
+    // 첫 번째 파라미터를 상수 버퍼 뷰로 초기화 (루트 매개변수 0번, b0 레지스터)
+    param[0].InitAsConstantBufferView(0); // 0번 -> b0 -> CBV
 
-	ComPtr<ID3DBlob> blobSignature;
-	ComPtr<ID3DBlob> blobError;
-	::D3D12SerializeRootSignature(&sigDesc, D3D_ROOT_SIGNATURE_VERSION_1, &blobSignature, &blobError);
-	device->CreateRootSignature(0, blobSignature->GetBufferPointer(), blobSignature->GetBufferSize(), IID_PPV_ARGS(&_signature));
+    // 두 번째 파라미터를 상수 버퍼 뷰로 초기화 (루트 매개변수 1번, b1 레지스터)
+    param[1].InitAsConstantBufferView(1); // 1번 -> b1 -> CBV
+
+    // 루트 서명 설명 구조체 초기화 (2개의 루트 파라미터를 사용)
+    D3D12_ROOT_SIGNATURE_DESC sigDesc = CD3DX12_ROOT_SIGNATURE_DESC(2, param);
+
+    // 루트 서명의 플래그 설정 (입력 조립기 단계 사용 가능)
+    sigDesc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+
+    // 직렬화된 루트 서명을 담을 Blob 선언
+    ComPtr<ID3DBlob> blobSignature;
+    // 에러 메시지를 담을 Blob 선언
+    ComPtr<ID3DBlob> blobError;
+
+    // 루트 서명을 직렬화 (D3D12RootSignatureDesc -> ID3DBlob)
+    ::D3D12SerializeRootSignature(&sigDesc, D3D_ROOT_SIGNATURE_VERSION_1, &blobSignature, &blobError);
+
+    // 직렬화된 루트 서명을 사용하여 실제 루트 서명을 생성
+    device->CreateRootSignature(0, blobSignature->GetBufferPointer(), blobSignature->GetBufferSize(), IID_PPV_ARGS(&_signature));
 }
