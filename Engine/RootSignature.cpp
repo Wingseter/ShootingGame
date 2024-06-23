97c4e5da13681850bde5239657e661cdf7c1ac72
kwon <wingseter@gmail.com>
2024-06-24 00:10:02 +0900

    Init mesh with index buffer

--- Engine/RootSignature.cpp
+++ Engine/RootSignature.cpp
@@ -14,9 +14,9 @@ void RootSignature::Init(ComPtr<ID3D12Device> device)
 
     // 루트 서명 설명 구조체 초기화 (2개의 루트 파라미터를 사용)
     D3D12_ROOT_SIGNATURE_DESC sigDesc = CD3DX12_ROOT_SIGNATURE_DESC(2, param);
-
     // 루트 서명의 플래그 설정 (입력 조립기 단계 사용 가능)
     sigDesc.Flags = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+    
     // 직렬화된 루트 서명을 담을 Blob 선언
     ComPtr<ID3DBlob> blobSignature;
     // 에러 메시지를 담을 Blob 선언
