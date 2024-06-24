ae43d41bb7fa2d1a099aa03ca28e5b4296f253fd
kwon <wingseter@gmail.com>
2024-06-24 13:54:31 +0900

    mapping mesh with texture

--- Engine/CommandQueue.h
+++ Engine/CommandQueue.h
@@ -14,19 +14,23 @@ public:
 	void RenderBegin(const D3D12_VIEWPORT* vp, const D3D12_RECT* rect);
 	void RenderEnd();
 
+	void FlushResourceCommandQueue();
+
 	ComPtr<ID3D12CommandQueue> GetCmdQueue() { return _cmdQueue; }
 	ComPtr<ID3D12GraphicsCommandList> GetCmdList() { return	_cmdList; }
+	ComPtr<ID3D12GraphicsCommandList> GetResourceCmdList() { return _resCmdList; }
 
 private:
-	// CommandQueue : DX12에 등장
-	// 외주를 요청할 때, 하나씩 요청하면 비효율적
-	// [외주 목록]에 일감을 차곡차곡 기록했다가 한 방에 요청하는 것
+	// CommandQueue : new Feature in DirextX12
+	// Collect tast and request at once
 	ComPtr<ID3D12CommandQueue>			_cmdQueue;
 	ComPtr<ID3D12CommandAllocator>		_cmdAlloc;
 	ComPtr<ID3D12GraphicsCommandList>	_cmdList;
 
-	// Fence : 울타리(?)
-	// CPU / GPU 동기화를 위한 간단한 도구
+	ComPtr<ID3D12CommandAllocator> _resCmdAlloc;
+	ComPtr<ID3D12GraphicsCommandList> _resCmdList;
+
+	// Fence: synchronize tools for cpu / gpu
 	ComPtr<ID3D12Fence>					_fence;
 	uint32								_fenceValue = 0;
 	HANDLE								_fenceEvent = INVALID_HANDLE_VALUE;
--- Engine/Mesh.cpp
+++ Engine/Mesh.cpp
@@ -11,18 +11,18 @@ void Mesh::Init(const vector<Vertex>& vertexBuffer, const vector<uint32>& indexB
 void Mesh::Render()
 {
 	CMD_LIST->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-	CMD_LIST->IASetVertexBuffers(0, 1, &_vertexBufferView); // Slot: (0~15)
+	CMD_LIST->IASetVertexBuffers(0, 1, &_vertexBufferView);
 	CMD_LIST->IASetIndexBuffer(&_indexBufferView);
 
-	{
-		D3D12_CPU_DESCRIPTOR_HANDLE handle = GEngine->GetCB()->PushData(0, &_transform, sizeof(_transform));
-		GEngine->GetTableDescHeap()->SetCBV(handle, CBV_REGISTER::b0);
-	}
+	D3D12_CPU_DESCRIPTOR_HANDLE handle = GEngine->GetCB()->PushData(0, &_transform, sizeof(_transform));
+	GEngine->GetTableDescHeap()->SetCBV(handle, CBV_REGISTER::b0);
+	GEngine->GetTableDescHeap()->SetSRV(_tex->GetCpuHandle(), SRV_REGISTER::t0);
 
 	GEngine->GetTableDescHeap()->CommitTable();
 
 	CMD_LIST->DrawIndexedInstanced(_indexCount, 1, 0, 0, 0);
 }
+
 
 void Mesh::CreateVertexBuffer(const vector<Vertex>& buffer)
 {
