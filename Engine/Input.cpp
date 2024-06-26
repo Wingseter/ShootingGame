4a0521fdeb6ce311578328a9f9e58759bd64d47e
kwon <wingseter@gmail.com>
2024-06-26 21:38:23 +0900

    fix input system for performance

--- Engine/Input.cpp
+++ Engine/Input.cpp
@@ -7,6 +7,7 @@ void Input::Init(HWND hwnd)
 	_states.resize(KEY_TYPE_COUNT, KEY_STATE::NONE);
 }
 
+
 void Input::Update()
 {
 	HWND hwnd = ::GetActiveWindow();
@@ -17,13 +18,19 @@ void Input::Update()
 
 		return;
 	}
-	
+
+	BYTE asciiKeys[KEY_TYPE_COUNT] = {};
+	if (::GetKeyboardState(asciiKeys) == false)
+		return;
+
 	for (uint32 key = 0; key < KEY_TYPE_COUNT; key++)
 	{
-		if (::GetAsyncKeyState(key) & 0x8000)
+		// 키가 눌려 있으면 true
+		if (asciiKeys[key] & 0x80)
 		{
 			KEY_STATE& state = _states[key];
 
+			// 이전 프레임에 키를 누른 상태라면 PRESS
 			if (state == KEY_STATE::PRESS || state == KEY_STATE::DOWN)
 				state = KEY_STATE::PRESS;
 			else
@@ -33,6 +40,7 @@ void Input::Update()
 		{
 			KEY_STATE& state = _states[key];
 
+			// 이전 프레임에 키를 누른 상태라면 UP
 			if (state == KEY_STATE::PRESS || state == KEY_STATE::DOWN)
 				state = KEY_STATE::UP;
 			else
